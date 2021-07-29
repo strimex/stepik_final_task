e46feb06db5f695bd2bcfb0c0247302f7876868e
strimex <deleteacc11@gmail.com>
2021-07-29 17:29:43 +0300

    Page Object

---
+++ pages/main_page.py
@@ -0,0 +1,6 @@
+from .base_page import BasePage
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element_by_css_selector("#login_link")
+        login_link.click()
